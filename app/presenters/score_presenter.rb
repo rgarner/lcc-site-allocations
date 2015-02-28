6a9ddf956b9e0560f5358228389554331807ee70
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-02-28 10:50:29 +0000

    Revert "Don't need to check string to present score"
    Yes, we do - we need class="vx"!
    
    This reverts commit 8863f3859c95c7750cb60bb94770ee33f122fa7e.

--- app/presenters/score_presenter.rb
+++ app/presenters/score_presenter.rb
@@ -2,6 +2,7 @@ require_relative 'presenter_base'
 
 class ScorePresenter < PresenterBase
   def css_class
-    "score v#{model.score.to_i}"
+    score_str = model.score == '0?' ? '0' : model.score
+    "score v#{score_str}"
   end
 end
