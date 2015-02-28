8863f3859c95c7750cb60bb94770ee33f122fa7e
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-02-28 09:05:38 +0000

    Don't need to check string to present score
    * Now we have Score#to_i

--- app/presenters/score_presenter.rb
+++ app/presenters/score_presenter.rb
@@ -2,7 +2,6 @@ require_relative 'presenter_base'
 
 class ScorePresenter < PresenterBase
   def css_class
-    score_str = model.score == '0?' ? '0' : model.score
-    "score v#{score_str}"
+    "score v#{model.score.to_i}"
   end
 end
