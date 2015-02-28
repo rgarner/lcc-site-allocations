8d7c0e7af7ca75c7a0abfc3025772b2b80f4eafe
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-02-28 09:05:57 +0000

    Show a total score for a site

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,2 +1,12 @@
 module SitesHelper
+  def total_css_class(total_score)
+    case total_score
+    when -58..-10 then 'very-negative'
+    when -10..-1 then 'negative'
+    when -0 then 'neutral'
+    when 1..10 then 'positive'
+    when 1..58 then 'very-positive'
+      else raise ArgumentError, "#{total_score} unbanded"
+    end
+  end
 end
