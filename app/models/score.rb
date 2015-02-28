3a1f43ceae19d3082ee74714a0f2d22e499ef2c8
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-02-28 07:40:44 +0000

    Add Score#to_i, Score#uncertain?
    * to_i gets numerical value from #score
    * uncertain? checks for '?' as last char in #score

--- app/models/score.rb
+++ app/models/score.rb
@@ -1,4 +1,17 @@
 class Score < ActiveRecord::Base
   belongs_to :site
   belongs_to :score_type
+
+  def to_i
+    case score
+    when '0?' then 0
+    when 'x' then nil
+    else
+      score.to_i
+    end
+  end
+
+  def uncertain?
+    score[-1] == '?'
+  end
 end
--- spec/models/score_spec.rb
+++ spec/models/score_spec.rb
@@ -1,5 +1,47 @@
 require 'rails_helper'
 
-RSpec.describe Score, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe Score, :type => :model do
+  describe '#to_i, #uncertain?' do
+    let(:score) { Score.new(score: score_str) }
+
+    subject { score.to_i }
+
+    context 'is -2' do
+      let(:score_str) { '-2' }
+      it { should eql(-2) }
+    end
+    context 'is -1' do
+      let(:score_str) { '-1' }
+      it { should eql(-1) }
+      it 'is not uncertain' do
+        expect(score.uncertain?).to eql(false)
+      end
+    end
+    context 'is x' do
+      let(:score_str) { 'x' }
+      it { should be_nil }
+      it 'is not uncertain' do
+        expect(score.uncertain?).to eql(false)
+      end
+    end
+    context 'is 0?' do
+      let(:score_str) { '0?' }
+      it { should eql(0) }
+      it 'is uncertain' do
+        expect(score.uncertain?).to eql(true)
+      end
+    end
+    context 'is 0' do
+      let(:score_str) { '0' }
+      it { should eql(0) }
+    end
+    context 'is 1' do
+      let(:score_str) { '1' }
+      it { should eql(1) }
+    end
+    context 'is 2' do
+      let(:score_str) { '2' }
+      it { should eql(2) }
+    end
+  end
 end
