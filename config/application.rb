e4715591d80f694b1cdb6a82bed3ee8ec5dea91a
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-04-08 13:09:42 +0300

    Hardwire to postgis adapter
    * The default DATABASE_URL parser will assign the 'postgres'
      adapter based on a postgres:// URL. This is not what we want.
      Give us 'postgis' instead.

--- config/application.rb
+++ config/application.rb
@@ -8,7 +8,29 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "sprockets/railtie"
-# require "rails/test_unit/railtie"
+
+# From https://devcenter.heroku.com/articles/postgis
+class ActiveRecordOverrideRailtie < Rails::Railtie
+  initializer "active_record.initialize_database.override" do |app|
+
+    ActiveSupport.on_load(:active_record) do
+      if url = ENV['DATABASE_URL']
+        ActiveRecord::Base.connection_pool.disconnect!
+        parsed_url = URI.parse(url)
+        config =  {
+          adapter:             'postgis',
+          host:                parsed_url.host,
+          encoding:            'unicode',
+          database:            parsed_url.path.split("/")[-1],
+          port:                parsed_url.port,
+          username:            parsed_url.user,
+          password:            parsed_url.password
+        }
+        establish_connection(config)
+      end
+    end
+  end
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
