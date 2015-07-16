be64109541d7447f0ae02479ef12770b7c6c9411
Russell Garner <rgarner@zephyros-systems.co.uk>
2015-07-16 15:42:05 +0100

    Remove extraneous space from "Under construction"

--- lib/site_allocations/import/allocations.rb
+++ lib/site_allocations/import/allocations.rb
@@ -3,26 +3,35 @@ require 'csv'
 module SiteAllocations
   module Import
     class Allocations < Struct.new(:filename)
-      def run!
-        CSV.read(filename, headers: true).each do |row|
-          Allocation.find_or_create_by!(plan_ref: row['Allocation Ref']) do |allocation|
-            # Allocation Ref,SHLAA Refs,Address,Capacity,Completed post-2012, Under construction,Not started,Area HA,Green/Brown,Status
-            allocation.address = row['Address']
-            allocation.capacity = row['Capacity']
-            allocation.completed_post_2012 = row['Completed post-2012']
-            allocation.under_construction = row[' Under construction']
-            allocation.not_started = row['Not started']
-            allocation.area_ha = row['Area HA']
-            allocation.green_brown = row['Green/Brown']
-            allocation.status = row['Status']
 
-            row['SHLAA Refs'].split('_').each do |shlaa_ref|
-              allocation.sites << Site.find_by!(shlaa_ref: shlaa_ref)
-              allocation.save!
+      def run!
+        Allocation.transaction do
+          CSV.read(filename, headers: true).each do |row|
+            Allocation.where(plan_ref: row['Allocation Ref']).first_or_create!.tap do |allocation|
+              allocation.address             = row['Address']
+              allocation.capacity            = row['Capacity']
+              allocation.completed_post_2012 = row['Completed post-2012']
+              allocation.under_construction  = row['Under construction']
+              allocation.not_started         = row['Not started']
+              allocation.area_ha             = row['Area HA']
+              allocation.green_brown         = row['Green/Brown']
+              allocation.status              = row['Status']
+
+              row['SHLAA Refs'].split('_').each do |shlaa_ref|
+                site = Site.find_by(shlaa_ref: shlaa_ref)
+                if site
+                  allocation.sites << site
+                  allocation.save!
+                else
+                  $stderr.puts "No site found for shlaa_ref: #{shlaa_ref}"
+                end
+              end
+
             end
           end
         end
       end
+
     end
   end
 end
